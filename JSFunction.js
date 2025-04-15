43db76b6243209fb2070373195e07a7f15af66b0
An-uriel <2aliviadoangel@gmail.com>
2025-04-15 15:58:14 +0800

    Create JSFunction.js

---
+++ JSFunction.js
@@ -0,0 +1,14 @@
+function negaToPosi(num) {
+    let Num;
+let nega = -1;
+if( num < 0) {
+Num = num * nega;
+}else{
+Num = num * nega; 
+}
+
+console.log(`${Num}`);
+return Num;
+}
+
+negaToPosi(4); 
